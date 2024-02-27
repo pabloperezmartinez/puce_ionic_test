fccbca0c1c3899105a1e98096dd9b99f344bf873
Pablo Pérez Martínez <paperez@puce.edu.ec>
2024-02-27 16:28:49 -0500

    Agregar interfaces

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page{
 
-  constructor() {}
+  public toastButtons = [
+    {
+      text: 'Leer más'
+    },
+  ];
+  constructor(private toastController: ToastController) {}
 
+  async ionViewDidEnter() {
+    const toast = await this.toastController.create({
+      message: 'Estimado miembro de la comunidad: Las clases se han suspendido desde el día 1 de marzo de 2024 indefinidamente debido al paro nacional',
+      color: 'danger',
+      duration: 10000,
+      position: 'top',
+      buttons: this.toastButtons
+    });
+
+    await toast.present();
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,6 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  highlightedDates = [
+    {
+      date: '2023-02-05',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2024-02-06',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2024-02-07',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2024-02-08',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2024-02-09',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2024-02-10',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2024-02-15',
+      textColor: '#09721b',
+      backgroundColor: '#c8e5d0',
+    },
+    {
+      date: '2024-02-20',
+      textColor: 'var(--ion-color-secondary-contrast)',
+      backgroundColor: 'var(--ion-color-secondary)',
+    },
+    {
+      date: '2024-02-23',
+      textColor: 'rgb(68, 10, 184)',
+      backgroundColor: 'rgb(211, 200, 229)',
+    },
+  ];
 
   constructor() {}
 
